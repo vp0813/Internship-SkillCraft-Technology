01b5ee5cfe20853902700e62be4576fe9ca24792
vp0813 <patravaishnavi61@gmail.com>
2026-01-10 13:56:53 +0530

    Image zoom when hover added

---
+++ SCT_WD_1/script.js
@@ -0,0 +1,27 @@
+let webname = document.getElementById("name");
+if(webname){
+    webname.onclick = function(){
+        window.location.href = "index.html";
+    }
+}
+
+let home = document.getElementById("home");
+if(home){
+    home.onclick = function(){
+        window.location.href = "index.html";
+    }
+}
+
+let light1 = document.getElementById("light");
+if(light1){
+    light1.onclick = function(){
+        document.body.style.backgroundColor = "white";
+    }
+}
+
+let light2 = document.getElementById("dark");
+if(light2){
+    light2.onclick = function(){
+        document.body.style.backgroundColor = "black";
+    }
+}
